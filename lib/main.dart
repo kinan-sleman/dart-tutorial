e615a2612e8c3705f45dc54feb55869a22f552cd
Kinan Sleman <kenan@oonatech.com>
2026-05-04 07:30:20 +0300

    variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
 void main() {
-  print('Hello, World!');
+  // variable name is declared and initialized with the value "kinan"
+  // variable rule is followed, which means the variable name is a valid identifier and does not contain any spaces or special characters
+  String name = "kinan";
+  int age = 30; // variable age is declared and initialized with the value 30
+  print(name);
+  print("Hello, $name!"); // using string interpolation to print a greeting message
+  print("You are $age years old."); // using string interpolation to print the age
 }
