0d2871b09f1ad58db4f8dbd4be9249475945ae37
Kinan Sleman <kenan@oonatech.com>
2026-05-04 07:56:20 +0300

    Non-nullable

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 void main() {
-  // -------------------- String DataType --------------------:
-  String name = "John Doe";
-  name = "Jane Doe"; // Reassigning a new value to the variable
-  print(name); // Output: Jane Doe
-
-  String FirstName = "John";
-  String LastName = "Doe";
-  print("My Full Name is: $FirstName $LastName"); // Using string interpolation
-  // -------------------- Int DataType --------------------:
-  int age = 30;
-  age = 31; // Reassigning a new value to the variable
-  print(age); // Output: 31
-  // -------------------- double DataType --------------------:
-  double weight = 3;
-  weight = 3.5; // Reassigning a new value to the variable  
-  print(weight); // Output: 3.5
-  // -------------------- bool DataType --------------------:
-  bool isStudent = true;
-  isStudent = false; // Reassigning a new value to the variable
-  print(isStudent); // Output: false
+  // String name;
+  // print(name);
+  // this cause this error:
+  // Error: Non-nullable variable 'name' must be assigned before it can be used.
+  // To fix this error, you can either initialize the variable with a default value or make it nullable by adding a '?' after the type. For example:
+  String? name;
+  print(name); // This will print 'null' since the variable is now nullable.
+  // print(name.length); This will cause an error (Error: Property 'length' cannot be accessed on 'String?' because it is potentially null.)
+  // To fix this error, you can use '!'
+  // print(name!.length); This will cause a runtime error (Unhandled exception: Null check operator used on a null value) because 'name' is null.
+  // To avoid this error, you can check if 'name' is not null before accessing its properties:
+  if (name != null) {
+    print(name.length);
+  } else {
+    print('name is null');
+  }
 }
