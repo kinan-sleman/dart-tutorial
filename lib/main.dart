81e615ee7b56deb3ad70a95a7459defd7b5fc097
Kinan Sleman <kenan@oonatech.com>
2026-05-04 07:50:50 +0300

    DataTypes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 void main() {
-  // variable name is declared and initialized with the value "kinan"
-  // variable rule is followed, which means the variable name is a valid identifier and does not contain any spaces or special characters
-  String name = "kinan";
-  int age = 30; // variable age is declared and initialized with the value 30
-  print(name);
-  print("Hello, $name!"); // using string interpolation to print a greeting message
-  print("You are $age years old."); // using string interpolation to print the age
+  // -------------------- String DataType --------------------:
+  String name = "John Doe";
+  name = "Jane Doe"; // Reassigning a new value to the variable
+  print(name); // Output: Jane Doe
+
+  String FirstName = "John";
+  String LastName = "Doe";
+  print("My Full Name is: $FirstName $LastName"); // Using string interpolation
+  // -------------------- Int DataType --------------------:
+  int age = 30;
+  age = 31; // Reassigning a new value to the variable
+  print(age); // Output: 31
+  // -------------------- double DataType --------------------:
+  double weight = 3;
+  weight = 3.5; // Reassigning a new value to the variable  
+  print(weight); // Output: 3.5
+  // -------------------- bool DataType --------------------:
+  bool isStudent = true;
+  isStudent = false; // Reassigning a new value to the variable
+  print(isStudent); // Output: false
 }
