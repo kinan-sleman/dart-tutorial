218f7bdfdadb17829308eaea3efd913137d6bd23
Kinan Sleman <kenan@oonatech.com>
2026-05-06 16:11:36 +0300

    Comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 void main() {
-  int a = 100;
-  print(a > 10 && a < 10);  // Logical AND
-  print('-----------------');
-  print(a > 10 || a < 10); // Logical OR
-  print('-----------------');
-  print(!(a > 10)); // Negation
-  print('-----------------');
-  print(a > 10 && a < 10 || a == 100); // Logical AND and OR
-  print('-----------------');
-  print(!(a < 10 || a < 10)); // Negation of Logical OR
-  print('-----------------');
-  print(!(a > 10 && a > 10)); // Negation of Logical AND
+  // One Line Comment
+  /*
+   * Multi Line Comment
+   * Another Line
+   * Third Line
+   * Fourth Line
+   * Fifth Line
+   * Sixth Line
+   * Seventh Line
+   * Eighth Line
+   * Ninth Line
+   * Tenth Line
+   * Eleventh Line
+   */  
 }
