8ffd269600a693d46a5e4e82c57a15693f6cd30d
Kinan Sleman <kenan@oonatech.com>
2026-05-06 16:10:28 +0300

    Logical Operators

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,14 @@
 void main() {
-  int a = 10;
-  print(a is int); // true
-  print(a is! int); // false
+  int a = 100;
+  print(a > 10 && a < 10);  // Logical AND
   print('-----------------');
-  String b = 'Hello';
-  print(b is String); // true
-  print(b is! String); // false
+  print(a > 10 || a < 10); // Logical OR
   print('-----------------');
-  int c = 10;
-  c ??= 20; // c is not null, so it remains 10
-  print(c); // 10
+  print(!(a > 10)); // Negation
   print('-----------------');
-  int? d; // d is null
-  d ??= 30; // d is null, so it gets assigned 30
-  print(d); // 30
+  print(a > 10 && a < 10 || a == 100); // Logical AND and OR
   print('-----------------');
-  int e = 20;
-  e += 10; // e becomes 30
-  print(e); // 30
+  print(!(a < 10 || a < 10)); // Negation of Logical OR
   print('-----------------');
-  int f = 5;
-  f *= 2; // f becomes 10
-  print(f); // 10
-  print('-----------------');
-  int g = 15;
-  g ~/= 3; // g becomes 5
-  print(g); // 5
-  print('-----------------');
-  double h = 10;
-  h /= 5; // h becomes 2
-  print(h); // 2
+  print(!(a > 10 && a > 10)); // Negation of Logical AND
 }
