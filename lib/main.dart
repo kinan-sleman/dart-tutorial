707803a2e56f78bcbc1cbe44af1d82fc9f4cd38b
Kinan Sleman <kenan@oonatech.com>
2026-05-06 16:20:26 +0300

    Break

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 void main() {
-  // Do While
-  int i = 0;
-  do {
+  // Break
+  for (int i = 0; i < 10; i++) {
+    if (i == 5) {
+      break; // Exit the loop when i is 5
+    }
     print(i);
-    i++;
-  } while (i < 5);
+  }
 }
