48d9b2ed2e10462d834f2143af33719ed230dcd4
Kinan Sleman <kenan@oonatech.com>
2026-05-06 16:29:44 +0300

    Number Operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 void main() {
-  // Continue 
-  for (var i = 0; i < 5; i++) {
-    if (i == 3) {
-      continue; // Skip the rest of the loop when i is 3
-    }
-    print('Iteration: $i');
-  }
+  // Number Operations
+  int a = 10;
+  // isFinite is a property that returns true if the number is finite
+  print(a.isFinite); // true
+  print('----------');
+  // isInfinite is a property that returns true if the number is infinite
+  print(a.isInfinite); // false
+  print('----------');
+  double b = 10 / 0;
+  print(b.isFinite); // false
+  print('----------');
+  print(b.isInfinite); // true
+  print('----------');
+  int c = -10;
+  // isNegative is a property that returns true if the number is negative
+  print(c.isNegative); // true
+  print('----------');
+  // isEven is a property that returns true if the number is even
+  print(c.isEven); // false
+  print('----------');
+  // isOdd is a property that returns true if the number is odd
+  print(c.isOdd); // true
+  print('----------');
+  // sign is a property that returns the sign of a number
+  print(c.sign); // -1
+  print(b.sign); // 1
+  print('---------');
+  // abs is method that returns the absolute value of a number
+  print(a.abs()); // 10
+  print(c.abs()); // 10
+  print('---------');
 }
