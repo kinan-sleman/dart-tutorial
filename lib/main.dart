6a9d5e7ab1d91cf87542043af378b89c06b6acf4
Kinan Sleman <kenan@oonatech.com>
2026-05-06 16:33:34 +0300

    String Method

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,31 @@
 void main() {
-  // Number Operations
-  int a = 10;
-  // isFinite is a property that returns true if the number is finite
-  print(a.isFinite); // true
-  print('----------');
-  // isInfinite is a property that returns true if the number is infinite
-  print(a.isInfinite); // false
-  print('----------');
-  double b = 10 / 0;
-  print(b.isFinite); // false
-  print('----------');
-  print(b.isInfinite); // true
-  print('----------');
-  int c = -10;
-  // isNegative is a property that returns true if the number is negative
-  print(c.isNegative); // true
-  print('----------');
-  // isEven is a property that returns true if the number is even
-  print(c.isEven); // false
-  print('----------');
-  // isOdd is a property that returns true if the number is odd
-  print(c.isOdd); // true
-  print('----------');
-  // sign is a property that returns the sign of a number
-  print(c.sign); // -1
-  print(b.sign); // 1
-  print('---------');
-  // abs is method that returns the absolute value of a number
-  print(a.abs()); // 10
-  print(c.abs()); // 10
-  print('---------');
+  // String Method 
+  String name = "Flutter is awesome";
+  print(name.length); // Output: 18
+  print(name.toUpperCase()); // Output: FLUTTER IS AWESOME
+  print(name.toLowerCase()); // Output: flutter is awesome
+  String firstName = "John";
+  String lastName = "Doe";
+  String fullName = firstName + " " + lastName;
+  print(fullName); // Output: John Doe
+  // concatenation using interpolation
+  String fullName2 = "$firstName $lastName";
+  print(fullName2); // Output: John Doe
+  // concatenation using interpolation with expression
+  String fullName3 = "${firstName.toUpperCase()} ${lastName.toUpperCase()}";
+  print(fullName3); // Output: JOHN DOE
+  String emptyString = "";
+  print(emptyString.isEmpty); // Output: true
+  print(emptyString.isNotEmpty); // Output: false
+  // Trim method
+  String stringWithSpaces = "   Hello World   ";
+  print(stringWithSpaces.trim()); // Output: Hello World
+  // compareTo method
+  String string1 = "apple";
+  String string2 = "banana";
+  print(string1.compareTo(string2)); // Output: -1 (because "apple" comes before "banana")
+  print(string2.compareTo(string1)); // Output: 1 (because "banana" comes after "apple")
+  // replaceAll method
+  String stringWithSpaces2 = "Hello World";
+  print(stringWithSpaces2.replaceAll(" ", "_")); // Output: Hello_World
 }
